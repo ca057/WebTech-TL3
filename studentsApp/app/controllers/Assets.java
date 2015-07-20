d251af3b7f3bda337b48534c2ae398a553463627
Christian Ost <ca@smo.de>
2015-07-20 23:37:48 +0200

    Made some changes for testing purposes.

---
+++ studentsApp/app/controllers/Assets.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import java.nio.file.Path;
+
+public class Assets {
+
+	public void at(Path path, String file) {
+		// TODO nicht sicher ob das so geht --> zwei Parameter in routes, zwei
+		// Parameter hier?!
+	}
+}
