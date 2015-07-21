a3d8a58e41c50d8cac2dfd85187eefa0d91e6b22
Christian Ost <ca@smo.de>
2015-07-21 13:47:23 +0200

    Fixed bug

--- studentsApp/app/controllers/Application.java
+++ studentsApp/app/controllers/Application.java
@@ -7,7 +7,7 @@ import views.html.index;
 public class Application extends Controller {
 
 	public static Result index() {
-		return ok(index.render("studentsApp is ready for developing, let´s get started!"));
+		return ok(index.render(""));
 	}
 
 }
