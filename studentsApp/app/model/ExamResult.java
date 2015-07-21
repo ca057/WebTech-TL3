c36796240c3d093e911e31c78d7b9cbe9383ee7e
simon.stemper@gmail.com <simon.stemper@gmail.com>
2015-07-21 22:33:02 +0200

    Added Javadoc to ExamResult.class

--- studentsApp/app/model/ExamResult.java
+++ studentsApp/app/model/ExamResult.java
@@ -2,6 +2,7 @@ package model;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -21,32 +22,65 @@ import play.db.ebean.Model;
 @SuppressWarnings("serial")
 @Entity
 public class ExamResult extends Model {
-
+	/**
+	 * The ID of the exam result. The column's name in the database is
+	 * accordingly set as "RESULT_ID".
+	 */
 	@Id
 	@Column(name = "RESULT_ID")
 	private Integer id;
-
+	/**
+	 * The name attribute of the exam result. Must not be <code>null</code>.
+	 */
 	@Required
 	private String name;
-
+	/**
+	 * The number of ECTS associated with this module group. Must not be
+	 * <code>null</code> or less than zero.
+	 */
 	@Required
 	private int ects;
-
+	/**
+	 * The grade granted to this exam. Must not be smaller or equal to zero or
+	 * greater than five.
+	 */
 	@Required
 	private float grade;
-
+	/**
+	 * The single module group associated with this exam result. Must not be
+	 * <code>null</code>. If exam result is removed, module group will be
+	 * updated.
+	 */
 	@Required
-	@ManyToOne(fetch = FetchType.LAZY)
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REFRESH)
 	@JoinColumn(name = "MODULE_ID")
 	private Module module;
 
+	/**
+	 * Constructor of {@code ExamResult}.
+	 * 
+	 * @param module
+	 *            Associated module. Must not be <code>null</code>.
+	 * @param name
+	 *            Name of the exam. Must not be <code>null</code>.
+	 * @param ects
+	 *            Number of ECTS associated with this module. Must not be
+	 *            <code>null</code> or smaller than zero.
+	 * @param grade
+	 *            Grade granted for this exam. Must not be <code>null</code>,
+	 *            smaller equal to zero or greater than five.
+	 */
 	public ExamResult(Module module, String name, int ects, float grade) {
 		Integer id = 0;
 		for (ExamResult result : all()) {
 			id = Math.max(id, result.getID());
 		}
 		this.id = id;
-		this.module = module;
+		if (module != null) {
+			this.module = module;
+		} else {
+			throw new IllegalArgumentException("ExamResult could not be initiated, Module not set");
+		}
 
 		setName(name);
 		setEcts(ects);
@@ -61,21 +95,14 @@ public class ExamResult extends Model {
 		return module;
 	}
 
-	public float getGrade() {
-		return grade;
-	}
-
-	public void setGrade(float grade) {
-		if (grade <= 0 || grade > 5.0) {
-			throw new IllegalArgumentException("Illegal arguments for grade.");
-		}
-		this.grade = grade;
-	}
-
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * @param name
+	 *            Name of the exam result. Must not be <code>null</code>.
+	 */
 	public void setName(String name) {
 		if (name == null || name.isEmpty()) {
 			throw new IllegalArgumentException("Illegal parameters for  name");
@@ -87,11 +114,32 @@ public class ExamResult extends Model {
 		return ects;
 	}
 
+	/**
+	 * @param ects
+	 *            Number of ECTS associated with this exam result. Must not be
+	 *            <code>null</code> or smaller than zero.
+	 */
 	public void setEcts(int ects) {
 		if (ects < 0) {
 			throw new IllegalArgumentException("Illegal parameters for ects");
 		}
 		this.ects = ects;
+	}
+
+	public float getGrade() {
+		return grade;
+	}
+
+	/**
+	 * @param grade
+	 *            Grade granted for this exam. Must not be <code>null</code>,
+	 *            smaller equal to zero or greater than five.
+	 */
+	public void setGrade(float grade) {
+		if (grade <= 0 || grade > 5.0) {
+			throw new IllegalArgumentException("Illegal arguments for grade.");
+		}
+		this.grade = grade;
 	}
 
 	/**
@@ -120,14 +168,36 @@ public class ExamResult extends Model {
 
 	public static Finder<Integer, ExamResult> find = new Finder<>(Integer.class, ExamResult.class);
 
+	/**
+	 * @return A complete {@code List} of all {@code ExamResult}s in database.
+	 */
 	public static List<ExamResult> all() {
 		return find.all();
 	}
 
+	/**
+	 * Create an {@code ExamResult} in database. The ExamResult will also be
+	 * associated with its {@code Module} automatically.
+	 * 
+	 * @param result
+	 *            The {@code ExamResult} to add. Must not be <code>null</code>.
+	 */
 	public static void create(ExamResult result) {
-		result.save();
+		if (result != null) {
+			result.save();
+			result.getModule().addExamResult(result);
+		} else {
+			throw new IllegalArgumentException("Result could not be added.");
+		}
 	}
 
+	/**
+	 * Delete a specific {@code ExamResult} from database. Reference on this
+	 * ExamResult by {@code Module} will also be removed.
+	 * 
+	 * @param id
+	 *            The ID of the {@code ExamResult} to delete from database.
+	 */
 	public static void delete(Integer id) {
 		find.ref(id).delete();
 	}
