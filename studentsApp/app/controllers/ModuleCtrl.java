e7b720154a86beb43b5b41e476998529d8b529c7
Christian Ost <ca@smo.de>
2015-07-21 22:49:03 +0200

    Finished routing, added lots of classes.

---
+++ studentsApp/app/controllers/ModuleCtrl.java
@@ -0,0 +1,32 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class ModuleCtrl extends Controller {
+
+	public ModuleCtrl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static Result newModule() {
+		return TODO;
+	}
+
+	public static Result changeModule() {
+		return TODO;
+	}
+
+	public static Result addModule(int id) {
+		return TODO;
+	}
+
+	public static Result editModule(long id) {
+		return TODO;
+	}
+
+	public static Result deleteModule(int id) {
+		return TODO;
+	}
+
+}
---
+++ studentsApp/app/controllers/ResultCtrl.java
@@ -0,0 +1,32 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class ResultCtrl extends Controller {
+
+	public ResultCtrl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static Result newResult() {
+		return TODO;
+	}
+
+	public static Result changeResult() {
+		return TODO;
+	}
+
+	public static Result addResult(int moduleid) {
+		return TODO;
+	}
+
+	public static Result editResult(int id) {
+		return TODO;
+	}
+
+	public static Result deleteResult(int id) {
+		return TODO;
+	}
+
+}
