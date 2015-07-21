dc0f09d6184e4ccf504b1cbd135ec2341b60cddb
simon.stemper@gmail.com <simon.stemper@gmail.com>
2015-07-21 21:47:23 +0200

    Updated model for play

--- studentsApp/app/model/Module.java
+++ studentsApp/app/model/Module.java
@@ -1,15 +1,23 @@
 package model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import play.data.validation.Constraints.Required;
+import play.db.ebean.Model;
 
+@SuppressWarnings("serial")
 @Entity
-public class Module {
+public class Module extends Model {
 
 	@Id
-	private int moduleID;
+	@Column(name = "MODULE_ID")
+	private Integer id;
 
 	@Required
 	private String name;
@@ -17,23 +25,22 @@ public class Module {
 	@Required
 	private int ects;
 
-	public Module(int moduleID, String name, int ects) {
+	@OneToMany(mappedBy = "module", cascade = CascadeType.REMOVE)
+	private List<ModuleResult> results;
+
+	public Module(String name, int ects) {
+		Integer id = 0;
+		for (Module module : all()) {
+			id = Math.max(id, module.getID());
+		}
+		this.id = id;
+
 		setName(name);
 		setEcts(ects);
-		this.moduleID = moduleID;
-		this.name = name;
-		this.ects = ects;
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		if (name == null || name.isEmpty()) {
-			throw new IllegalArgumentException("Illegal parameters for  name");
-		}
-		this.name = name;
+	public int getID() {
+		return id;
 	}
 
 	public int getEcts() {
@@ -47,7 +54,57 @@ public class Module {
 		this.ects = ects;
 	}
 
-	public int getModuleID() {
-		return moduleID;
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		if (name == null || name.isEmpty()) {
+			throw new IllegalArgumentException("Illegal parameters for name");
+		}
+		this.name = name;
+	}
+
+	public void addModuleResult(ModuleResult result) {
+		this.results.add(result);
+	}
+
+	/**
+	 * Get the average grade for results in this module.
+	 * 
+	 * <p>
+	 * The average grade is determined as follows:
+	 * <p>
+	 * The sum of all grades multiplied by their respective number of ECTS is
+	 * divided by the overall number of ECTS.
+	 * 
+	 * @return The average grade as {@code Float}, or <code>0</code> if no ECTS
+	 *         were achieved yet.
+	 * @return
+	 */
+	public Float getAverageGradeOfModule() {
+		float sumOfGradeTimesEcts = 0;
+		float sumOfEcts = 0;
+
+		for (ModuleResult result : this.results) {
+			sumOfGradeTimesEcts += result.getGrade() * result.getEcts();
+			sumOfEcts += result.getEcts();
+		}
+
+		return (sumOfEcts != 0) ? sumOfGradeTimesEcts / sumOfEcts : 0;
+	}
+
+	public static Finder<Integer, Module> find = new Finder<Integer, Module>(Integer.class, Module.class);
+
+	public static List<Module> all() {
+		return find.all();
+	}
+
+	public static void create(Module module) {
+		module.save();
+	}
+
+	public static void delete(Integer id) {
+		find.ref(id).delete();
 	}
 }
--- studentsApp/app/model/ModuleResult.java
+++ studentsApp/app/model/ModuleResult.java
@@ -1,38 +1,124 @@
 package model;
 
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import play.data.validation.Constraints.Required;
+import play.db.ebean.Model;
 
+@SuppressWarnings("serial")
 @Entity
-public class ModuleResult extends Module {
+public class ModuleResult extends Model {
 
 	@Id
-	private int resultID;
+	@Column(name = "RESULT_ID")
+	private Integer id;
+
+	@Required
+	private String name;
+
+	@Required
+	private int ects;
 
 	@Required
 	private float grade;
 
-	public ModuleResult(int resultID, String name, int ects, float grade, int moduleID) {
-		super(moduleID, name, ects);
+	@Required
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "MODULE_ID")
+	private Module module;
+
+	public ModuleResult(int resultID, Module module, String name, int ects, float grade) {
+		this.id = resultID;
+		this.module = module;
+
+		setName(name);
+		setEcts(ects);
 		setGrade(grade);
-		this.resultID = resultID;
-		this.grade = grade;
 	}
 
-	public void setGrade(float grade) {
-		if (grade <= 0 || grade > 5.0) {
-			throw new IllegalArgumentException("Illegal arguments for result.");
-		}
-		this.grade = grade;
+	public int getResultID() {
+		return id;
+	}
+
+	public Module getModule() {
+		return module;
 	}
 
 	public float getGrade() {
 		return grade;
 	}
 
-	public int getResultID() {
-		return resultID;
+	public void setGrade(float grade) {
+		if (grade <= 0 || grade > 5.0) {
+			throw new IllegalArgumentException("Illegal arguments for grade.");
+		}
+		this.grade = grade;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		if (name == null || name.isEmpty()) {
+			throw new IllegalArgumentException("Illegal parameters for  name");
+		}
+		this.name = name;
+	}
+
+	public int getEcts() {
+		return ects;
+	}
+
+	public void setEcts(int ects) {
+		if (ects < 0) {
+			throw new IllegalArgumentException("Illegal parameters for ects");
+		}
+		this.ects = ects;
+	}
+
+	/**
+	 * Get the average grade determined by all results.
+	 * 
+	 * <p>
+	 * The total average grade is determined as follows:
+	 * <p>
+	 * The sum of all grades multiplied by their respective number of ECTS is
+	 * divided by the overall number of ECTS.
+	 * 
+	 * @return The total average grade as {@code Float}, or <code>0</code> if no
+	 *         ECTS were achieved yet.
+	 */
+	public Float getTotalAverageGrade() {
+		float sumOfGradeTimesEcts = 0;
+		float sumOfEcts = 0;
+
+		for (ModuleResult result : all()) {
+			sumOfGradeTimesEcts += result.getGrade() * result.getEcts();
+			sumOfEcts += result.getEcts();
+		}
+
+		return (sumOfEcts != 0) ? sumOfGradeTimesEcts / sumOfEcts : 0;
+	}
+
+	public static Finder<Integer, ModuleResult> find = new Finder<>(Integer.class, ModuleResult.class);
+
+	public static List<ModuleResult> all() {
+		return find.all();
+	}
+
+	public static void create(ModuleResult result) {
+		result.save();
+	}
+
+	public static void delete(Integer id) {
+		find.ref(id).delete();
 	}
 }
