c4d9b90a9660447197d508404e45bb87b8f05f8f
Christian Ost <ca@smo.de>
2015-07-21 14:42:13 +0200

    Added some stuff

--- studentsApp/app/controllers/Application.java
+++ studentsApp/app/controllers/Application.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
@@ -7,7 +10,13 @@ import views.html.index;
 public class Application extends Controller {
 
 	public static Result index() {
-		return ok(index.render(""));
+		return ok(index.render("", generateListWithAllResults()));
+	}
+
+	private static List<ModuleGroup> generateListWithAllResults() {
+		List<ModuleGroup> allModulesAndResults = new ArrayList<ModuleGroup>();
+
+		return allModulesAndResults;
 	}
 
 }
