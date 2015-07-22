9b94e2aa4ee1124a1f9addc8f6ae073f503c1fd2
Christian Ost <ca@smo.de>
2015-07-22 15:42:15 +0200

    Fixed error

--- studentsApp/app/controllers/ResultCtrl.java
+++ studentsApp/app/controllers/ResultCtrl.java
@@ -15,10 +15,6 @@ public class ResultCtrl extends Controller {
 		// TODO Auto-generated constructor stub
 	}
 
-	public static Result examResults() {
-		return TODO;
-	}
-
 	public static Result newResult() {
 		return ok(views.html.addresult.render(""));
 	}
@@ -27,12 +23,17 @@ public class ResultCtrl extends Controller {
 		return ok(views.html.changeresult.render());
 	}
 
+	public static Result selectResultEditingMode() {
+		return TODO;
+	}
+
 	public static Result addResult() {
 		DynamicForm dynamicForm = Form.form().bindFromRequest();
 		String name = dynamicForm.get("name");
 		int moduleId = Integer.parseInt(dynamicForm.get("module"));
 		int ects = 0;
 		// check if ects is number
+		System.out.println("1");
 		try {
 			ects = Integer.parseInt(dynamicForm.get("ects"));
 		} catch (NumberFormatException e) {
@@ -40,6 +41,7 @@ public class ResultCtrl extends Controller {
 					.render("Die eingegebenen ECTS-Punkte sind keine Zahl und konnten nicht verarbeitet werden."));
 		}
 		// check if grade is number
+		System.out.println("2");
 		float grade = 0;
 		try {
 			grade = Float.valueOf(dynamicForm.get("grade"));
@@ -48,11 +50,13 @@ public class ResultCtrl extends Controller {
 					.render("Die eingegebene Note ist keine Zahl und konnte nicht verarbeitet werden."));
 		}
 		// check if grade and ects are larger than 0
+		System.out.println("3");
 		if (ects < 0 || grade <= 0) {
 			return ok(views.html.addresult
 					.render("Die eingegebenen ECTS-Punkte dürfen nicht negativ und die Note muss größer als 0 sein."));
 		} else {
 			ExamResult.create(new ExamResult(Module.getModuleById(moduleId), name, ects, grade));
+			System.out.println("finished");
 			return Application.index();
 		}
 	}
